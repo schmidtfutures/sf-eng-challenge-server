c60709fc781efe6139040e75942bc3ea710da1e1
Seth Martin <seth@pop-os.localdomain>
2022-07-03 20:33:05 -0400

    Creates basic server for data

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import express, { Application, Request, Response } from 'express';
 import * as swaggerUi from 'swagger-ui-express';
 import * as spec from './spec.json';
+import getMatches from './controller';
 
 const app: Application = express();
 const port = 3000;
@@ -25,6 +29,18 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(spec));
  * */
 app.get('/health', (req: Request, res: Response) => {
   res.json({ healthy: true });
+});
+
+// Health endpoint
+/**
+ * @param {Request} req - Express request
+ * @param {Response} res - Express response
+ * */
+app.get('/matches', (req: Request, res: Response) => {
+  const { userId } = req.query;
+
+  const users = getMatches(userId);
+  res.json({ users });
 });
 
 app.listen(port, () => {
